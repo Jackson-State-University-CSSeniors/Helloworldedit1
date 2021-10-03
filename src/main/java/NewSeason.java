8b0509e328c48e3de6ed5a2691057925c464b753
Timothy Stewart <timothy.stewart18@yahoo.com>
2021-10-03 18:39:04 -0500

    Adding HELLO WORLD!!

---
+++ src/main/java/NewSeason.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author TimothyStewart
+ */
+import java.util.Scanner;
+public class NewSeason {
+    public static void main(String [] arg){
+     System.out.println("Hello World!");
+     System.out.println("My name is Timothy Stewart");
+
+}
+}
